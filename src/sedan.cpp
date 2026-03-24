bbdb7c9024bf0c78e8faf7ee9902e79a9b5b4649
Christianp05 <chris@chrisLaptop.localdomain>
2026-03-24 14:52:11 +0100

    added solutions

--- src/sedan.cpp
+++ src/sedan.cpp
@@ -5,32 +5,41 @@ static const std::array<double, 7> gear_ratios = {-1, 1, 2, 3, 4, 5, 6};
 
 Sedan::Sedan() : current_gear(0)
 {
+
 }
 
 void Sedan::shift_up()
 {
+    //shifter up
+   this->current_gear++;
 }
 
 void Sedan::shift_down()
 {
+    //shifter ned 
+    this->current_gear--;
 }
 
 double Sedan::get_torque(double engine_rpm, double throttle)
 {
-    return 0;
+    //formlen fra assignment
+    return 100 * throttle;
 }
 
 double Sedan::get_weight()
 {
-    return 1;
+    //det er en toyota aygo vi arbejder med
+    return 105;
 }
 
 double Sedan::get_wheel_radius()
 {
-    return 1;
+    //med meget små hjul
+    return 0.3;
 }
 
 double Sedan::get_current_gear_ratio()
 {
-    return 1;
+    //bruger gearet som index i gearatioerne
+    return gear_ratios[current_gear];
 }
